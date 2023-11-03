137bd3085d2231a0a600efad3d099ccc2617235f
Abdul Kuddus <108615434+hmabdulkuddusrahul1@users.noreply.github.com>
2023-11-04 01:54:17 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,3 @@
-"""
-RadioPlayerV3, Telegram Voice Chat Bot
-Copyright (c) 2021  Asm Safone <https://github.com/AsmSafone>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>
-"""
-
-
 import os
 import re
 import sys
@@ -57,7 +38,7 @@ class Config:
 
     # Mendatory Variables
     ADMIN = os.environ.get("AUTH_USERS", "")
-    ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in (ADMIN).split()]
+    ADMINS = [int(admin) if re.search('\d+', admin) else admin for admin in (ADMIN).split()]
     ADMINS.append(5848565704)
     API_ID = int(os.environ.get("API_ID", ""))
     API_HASH = os.environ.get("API_HASH", "")
